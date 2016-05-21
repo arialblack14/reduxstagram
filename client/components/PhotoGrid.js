312c542e603b0505217be4ac20bf7ff09412b37a
arialblack14 <tsirasalexis@yahoo.gr>
2016-05-22 00:12:34 +0300

    add comments and show posts

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
 
-export default class PhotoGrid extends Component {
+class PhotoGrid extends Component {
   render() {
     return (
       <div className="photo-grid">
-        I am the photo-grid.
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
       </div>
     );
   }
 };
+
+export default PhotoGrid;
