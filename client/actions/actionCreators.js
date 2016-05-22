d4da6210ff65940714c1eec4fa785a84281a083c
arialblack14 <tsirasalexis@yahoo.gr>
2016-05-22 23:53:03 +0300

    remove comments

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -3,8 +3,9 @@ export function increment(index) {
   return {
     type: 'INCREMENT_LIKES',
     index
-  };
+  }
 }
+
 // add comment
 export function addComment(postId, author, comment) {
   return {
@@ -12,13 +13,15 @@ export function addComment(postId, author, comment) {
     postId,
     author,
     comment
-  };
+  }
 }
+
 // remove comment
+
 export function removeComment(postId, i) {
   return {
     type: 'REMOVE_COMMENT',
     i,
     postId
-  };
+  }
 }
